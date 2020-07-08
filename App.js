78cf3f3f2a68c855cd39758ea630fb6f39292906
Praveen Tiwari <sp2tiwari@gmail.com>
2020-07-08 21:09:24 +0530

    Google Login

---
+++ App.js
@@ -0,0 +1,124 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
+class App extends React.Component{
+
+   componentDidMount(){
+    getCurrentUser = () => {
+      const currentUser = GoogleSignin.getCurrentUser();
+      console.log("current user response",currentUser);
+      this.setState({ currentUser });
+    };
+
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId:'459075606066-stgvip0mk9b6890q70s058khmdfinldt.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      //offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      //hostedDomain: '', // specifies a hosted domain restriction
+      //loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+      //forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
+      //accountName: '', // [Android] specifies an account name on the device that should be used
+      //iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    });
+
+  }
+
+  signIn =  () => {
+    try {
+     GoogleSignin.hasPlayServices();
+      const userInfo =  GoogleSignin.signIn();
+      this.setState({ userInfo });
+      console.log("response of google login",userInfo);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+  render(){
+  return (
+  <View style={{alignItems:'center',justifyContent:'center',marginTop:20}}>
+    <Text style={{fontSize:30}}>
+      Google Login
+    </Text>
+    <GoogleSigninButton
+    style={{ width: 192, height: 48 }}
+    size={GoogleSigninButton.Size.Wide}
+    color={GoogleSigninButton.Color.Dark}
+    onPress={this.signIn}
+     />
+
+  </View>
+  );
+}
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
